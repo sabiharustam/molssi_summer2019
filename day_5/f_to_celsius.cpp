c1bdff11f95908040d797ab7859fe485b871e12d
Sabiha Rustam <sabiha3@uw.edu>
2019-07-26 10:35:45 -0500

    QM project from last year and this year.

--- day_5/f_to_celsius.cpp
+++ day_5/f_to_celsius.cpp
@@ -2,16 +2,24 @@
 
 double f_to_celsius(double f_temp);   // this is function declaration.
 
-int main(void)
-{
-	double f = 100.0;
-	double c = f_to_celsius(f);
-	std::cout << "F = " << f << "\nC = " << c << std::endl;
 
-	return 0;
-}
 
 double f_to_celsius(double f_temp)
 {
     return (f_temp - 32.0)/1.8;
+}
+
+double celsius_to_f(double c_temp)
+{
+    return (c_temp * 9/5) + 32;
+}
+
+int main(void)
+{
+
+	double c = 37.7778;
+	double f = celsius_to_f(c);
+	std::cout << "F = " << f << "\nC = " << c << std::endl;
+
+	return 0;
 }
