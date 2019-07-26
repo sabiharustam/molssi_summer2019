df4a96fd2941fa498ab283b6c6e381568f3f9bd5
Sabiha Rustam <sabiha3@uw.edu>
2019-07-26 09:38:17 -0500

    day_5 instruction on c++

---
+++ day_5/hello_world.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+// including iostream to use cout.  Header.
+
+int main(void)
+{
+	// This is one line comment. Start with double slash.
+	/* This can go over 
+	multiple line
+	wiithout any breaking. end with
+	*/
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
