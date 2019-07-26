19cc5508d9633d2c3ceba23f756ea0cf47904ac3
Sabiha Rustam <sabiha3@uw.edu>
2019-07-26 09:58:47 -0500

    error on constant

--- day_5/hello_world.cpp
+++ day_5/hello_world.cpp
@@ -6,8 +6,18 @@ int main(void)
 	// This is one line comment. Start with double slash.
 	/* This can go over 
 	multiple line
-	wiithout any breaking. end with
+	without any breaking. end with
 	*/
 	std::cout << "Hello, world!" << std::endl;
+
+	int a = 10;
+	a = 1.9
+	std::cout << "a is" << a << std::endl;
+	// This is from c to c++. Maybe you can add an error message.
+
+	// in c++ you can declare variable to be constants.
+	const double d = 10.0;
+	d = 1.0;
+
 	return 0;
 }
