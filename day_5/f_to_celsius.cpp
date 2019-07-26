147c96d864c04ce6e0db8b176e9adebfdb1605e5
Sabiha Rustam <sabiha3@uw.edu>
2019-07-26 13:22:14 -0500

    example of vector

--- day_5/f_to_celsius.cpp
+++ day_5/f_to_celsius.cpp
@@ -3,20 +3,17 @@
 # include "temperature.hpp"
 
 
-void count(int max)
-{
-    for(int i = 0; i < max; i++)
-    // first is initialization, second is test, third is what to execute.
-    // what this function does is it will print i in every step till
-    // i reaches the max.
-        std::cout << "i is" << i << std::endl;
-    // i++ is equal to i+=1 or ++i is the same.
-}
-
 int main(void)
 {
     std::vector<double> my_vector;
     my_vector.push_back(3.1415);
     std::cout << "0th element is " << my_vector[0] << std::endl;
+    std::cout << "0th element is " << my_vector.at(0) << std::endl;
+
+    std::cout << "1st element is " << my_vector[1] << std::endl;
+    std::cout << "1st element is " << my_vector.at(1) << std::endl;
+
+    std::cout << "My vector has " << my_vector.size() << "elements" <<
+    std::endl;
 	return 0;
 }
