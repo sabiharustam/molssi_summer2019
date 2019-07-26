4fc38c14df5d6ac90e6114fef8f189631b211c3f
Sabiha Rustam <sabiha3@uw.edu>
2019-07-26 10:24:28 -0500

    added function declaration

--- day_5/f_to_celsius.cpp
+++ day_5/f_to_celsius.cpp
@@ -1,9 +1,6 @@
 # include <iostream>
 
-double f_to_celsius(double f_temp)
-{
-    return (f_temp - 32.0)/1.8;
-}
+double f_to_celsius(double f_temp);   // this is function declaration.
 
 int main(void)
 {
@@ -12,4 +9,9 @@ int main(void)
 	std::cout << "F = " << f << "\nC = " << c << std::endl;
 
 	return 0;
+}
+
+double f_to_celsius(double f_temp)
+{
+    return (f_temp - 32.0)/1.8;
 }
