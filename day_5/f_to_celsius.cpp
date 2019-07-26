dfa814837bf980760433b84506ec9f966004baaa
Sabiha Rustam <sabiha3@uw.edu>
2019-07-26 14:31:35 -0500

    adding vector and matrixs

--- day_5/f_to_celsius.cpp
+++ day_5/f_to_celsius.cpp
@@ -10,6 +10,20 @@
 
 int main(void)
 {
+    Eigen::MatrixXd mat(2,3);
+    Eigen::VectorXd vec(3);
+
+    mat(0,0) = 1.0;
+    mat(0,1) = 2.0;
+    mat(0,2) = 3.0;
+    mat(1,0) = 4.0;
+    mat(1,1) = 5.0;
+    mat(1,2) = 6.0;
+
+    vec(0) = vec(1) = vec(2) = 1.0;
+
+    std::cout << "v.v = " << vec.dot(vec) << std::endl;
+    std::cout << mat << std::endl;
 
 	return 0;
 }
