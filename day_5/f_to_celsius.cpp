5325328fb9f1a0ed0aaa43db7d1f1cf34fd32b38
Sabiha Rustam <sabiha3@uw.edu>
2019-07-26 13:51:43 -0500

    added print function

--- day_5/f_to_celsius.cpp
+++ day_5/f_to_celsius.cpp
@@ -2,21 +2,40 @@
 # include <vector>
 # include "temperature.hpp"
 
-
+void print_vector(std::vector<double> vec)
+{
+    //for(size_t i = 0; i < vec.size(); i++)
+    //    std::cout << "Element " << i << " is" << vec[i] << std::endl;
+    // range-based for loop
+    for(auto it : vec)
+        std::cout << it << std::endl;
+}
 int main(void)
 {
     std::vector<double> my_vector;
-    for(int i = 0; i < 10; i++)
-        my_vector.push_back(3.1415);
+
+    my_vector.push_back(3.1415);
 
     std::cout << "0th element is " << my_vector[0] << std::endl;
     std::cout << "0th element is " << my_vector.at(0) << std::endl;
 
     std::cout << "1st element is " << my_vector[1] << std::endl;
-    std::cout << "1st element is " << my_vector.at(1) << std::endl;
+    //std::cout << "1st element is " << my_vector.at(1) << std::endl;
 
-    std::cout << "My vector has " << my_vector.size() << "elements" <<
-    std::endl;
+    std::vector<double> multiples;
+    for(int i = 0; i < 6; i++)
+    {
+        double k = 3.1415 * i;
+        multiples.push_back(k);
+    }
+
+    /*for(int i = 0; i < 5; i++)
+    {
+        std::cout << multiples[i] << std::endl;
+    }
+    std::cout << "My vector has " << multiples.size() << " elements" <<
+    std::endl;*/
+    print_vector(multiples);
 	return 0;
 }
 
