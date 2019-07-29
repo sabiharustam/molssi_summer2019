454742fe5e3a65059a44802c1611721853ec7015
Sabiha Rustam <sabiha3@uw.edu>
2019-07-29 13:25:43 -0500

    added other functions to be excicuted in python

--- day_5/export.cpp
+++ day_5/export.cpp
@@ -5,4 +5,6 @@ PYBIND11_MODULE(sss_cpp, m)
 {
     m.doc() = "This is example c++ module called from python";
     m.def("f_to_celsius", f_to_celsius, "Convert fahrenheit to celsius");
+    m.def("c_to_k", c_to_k, "Convert celsius to kelvin");
+    m.def("f_to_kelvin", f_to_kelvin, "Convert fahrenheit to kelvin");
 }
