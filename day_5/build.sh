8d8754cbabef032d885a44fa6703b651b713d91b
Sabiha Rustam <sabiha3@uw.edu>
2019-07-29 11:46:07 -0500

    create first compilables for .so file. Changed temperature.cpp and hpp file to what instrusture shared.

---
+++ day_5/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+g++ -fPIC --shared -std=c++11 -Wall temperature.cpp export.cpp \
+	-I${CONDA_PREFIX}/include \
+	-I${CONDA_PREFIX}/include/python3.7m \
+	-I${CONDA_PREFIX}/include/eigen3 \
+	-L${CONDA_PREFIX}/lib -lpython3.7m \
+	-o sss_cpp.so
--- day_5/temperature.hpp
+++ day_5/temperature.hpp
@@ -1,17 +1,13 @@
-
 #pragma once
 
 double f_to_celsius(double f_temp);
-double celsius_to_f(double c_temp);
-double celsius_to_kelvin(double c_temp);
+
+double c_to_k(double c_temp);
+
 double f_to_kelvin(double f_temp);
 
-bool check_temperature(double f_temp)
-{
-    double k = f_to_kelvin(f_temp);
-    if (k <= 0.0 || k > 1.0e6)
-        return false;
+// Returns false if f_temp is unphysical
+bool check_temperature(double f_temp);
 
-    else
-        return true;
-}
+void count(int max);
+
